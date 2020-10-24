dfc8904e48acb18f7a9556911a3518ed79b73ebd
Oshan Wickramarachchi <51901060+Oshanwick@users.noreply.github.com>
2020-10-25 01:18:31 +0530

    Add timestamp for each message

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React, { useRef, useState } from 'react';
-import './App.css';
-
-import firebase from 'firebase/app';
+import React, { useRef, useState } from "react";
+import "./App.css";
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import 'firebase/firestore';
-import 'firebase/auth';
 import 'firebase/analytics';
+import { FixedSizeList as List } from "react-window";
+import InfiniteLoader from "react-window-infinite-loader";
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
-  // your config
+//yourkey
 })
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
-
 
 function App() {
-
   const [user] = useAuthState(auth);
-
   return (
     <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
+      <header className="App-header">
         <SignOut />
       </header>
-
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
-
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
+
+
+
+
 
 function SignIn() {
 
@@ -47,28 +46,32 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
   )
-
 }
+
+
 
 function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>)
+
   )
 }
+
 
 
 function ChatRoom() {
-  const dummy = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const dummy = useRef()
+  const messagesRef = firestore.collection("meesages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
   const [formValue, setFormValue] = useState('');
-
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -79,45 +82,52 @@ function ChatRoom() {
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
     setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
 
-  return (<>
-    <main>
+    dummy.current.scroll({ behavior: 'smooth' });
+  };
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+  return (
+    <>
+      <main>
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
 
-      <span ref={dummy}></span>
 
-    </main>
 
-    <form onSubmit={sendMessage}>
 
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Type a message" />
 
-    </form>
-  </>)
+        <button type="submit" disabled={!formValue}>🕊️</button>
+      </form>
+
+
+    </>
+  )
 }
-
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
   return (<>
+
+
+
+
+
     <div className={`message ${messageClass}`}>
       <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
       <p>{text}</p>
-    </div>
+    </div>)
+
   </>)
 }
-
-
 export default App;
