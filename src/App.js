fd3f52fa5ea02559cb198d2f6452be5fd9fea4f7
Hussain Aqeel <69085853+huss-a@users.noreply.github.com>
2021-05-01 13:51:46 +0300

    Change limit
    Changed `limit()` to `limitToLast()` to show the **latest** 25 messages

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function SignOut() {
 function ChatRoom() {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt').limitToLast(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
