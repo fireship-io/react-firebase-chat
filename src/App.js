7e63858dc3346fdaf2dd3b2b22955c832322c725
Yasir <yhossain@gmail.com>
2022-05-09 22:48:37 -0400

    updated chat layout

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import "firebase/compat/analytics";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+import moment from 'moment';
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth(firebaseApp);
 const db = firebase.firestore(firebaseApp);
@@ -65,13 +67,9 @@ function ChatRoom() {
   const dummy = useRef();
 
   const messagesRef = db.collection('messages');
-  
   const query = messagesRef.orderBy('createdAt').limit(50);
-
   const [messages] = useCollectionData(query, { idField: 'id' });
-
   const [formValue, setFormValue] = useState('');
-
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -108,15 +106,22 @@ function ChatRoom() {
 }
 
 function ChatMessage(props) {
-  const { text, uid, photoURL, displayName } = props.message;
+  const { text, uid, photoURL, displayName, createdAt } = props.message;
+
+  // console.log(props.message);
+  // console.log(createdAt.seconds);
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const userName = displayName ? displayName : 'User';
 
   return (<>
-    <div className={`messageContainer ${messageClass}`}>
+    <div className={`message ${messageClass}`}>
       <img alt="avatar" className="avatar" src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{ displayName ? displayName : 'Random User' }</p>
-      <p>{text}</p>
+      <div className="messageContainer">
+        <p className="username">{ userName }</p>
+        <p className="message">{text}</p>
+        <p className="date">{moment(createdAt.seconds).add(3, 'days').calendar()}</p>
+      </div>
     </div>
   </>)
 }
