2f44b406c3831658a95f02709319bd4dbe38403d
PrAnAvViNaYaKjAdHaV <88928396+PrAnAvViNaYaKjAdHaV@users.noreply.github.com>
2022-09-04 21:18:16 +0530

    Upgrading v8 to new modular sdk (compact)
    As per the new version update 
     - updated import statements to compact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/analytics';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
