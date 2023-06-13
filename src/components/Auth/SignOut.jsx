5689ca18e681fb1fab37858378b487bd36c33b1c
Mustafa Alparslan <mustibatualp@gmail.com>
2023-06-13 11:56:34 +0000

    Update 6 fiRefactored Files for Enhanced Code Clarityles

---
+++ src/components/Auth/SignOut.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { auth } from "firebase";
+
+export default function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
---
+++ src/components/Chat/ChatRoom.jsx
@@ -0,0 +1,51 @@
+import firebase, { auth, firestore } from "firebase";
+import React from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import ChatMessage from "./ChatMessage";
+
+export default function ChatRoom() {
+  const [formValue, setFormValue] = React.useState("");
+  const dummy = React.useRef();
+
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+    if (formValue.trim()) {
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL,
+      });
+    }
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          maxLength={250}
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="say something nice"
+        />
+        <button type="submit" disabled={!formValue}>
+          🕊️
+        </button>
+      </form>
+    </>
+  );
+}
